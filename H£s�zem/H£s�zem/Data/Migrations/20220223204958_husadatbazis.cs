fafa930faa4ae67941d474983fa27d1e9e78400e
spen0t <71476085+spen0t@users.noreply.github.com>
2022-03-03 05:19:38 +0100

    masodikcommit

---
+++ H£s�zem/H£s�zem/Data/Migrations/20220223204958_husadatbazis.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Húsüzem.Data.Migrations
+{
+    public partial class husadatbazis : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Husika",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Terméknév = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ár = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Alapanyag = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GyártásIdeje = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Husika", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Husika");
+        }
+    }
+}
---
+++ H£s�zem/H£s�zem/Models/Husika.cs
@@ -0,0 +1,16 @@
+﻿namespace Húsüzem.Models
+{
+    public class Husika
+    {
+        public int Id { get; set; }
+        public string Terméknév { get; set; }
+        public string Ár { get; set; }
+        public string Alapanyag { get; set; }
+        public string GyártásIdeje { get; set; }
+
+        public Husika()
+        {
+
+        }
+    }
+}
